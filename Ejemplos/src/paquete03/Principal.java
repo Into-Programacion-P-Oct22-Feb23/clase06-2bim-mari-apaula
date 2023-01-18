b6d27084bfcaedccc6c941f34e81caf57a1d2ad7
Maria Paula Montaño <mpmontano1@utpl.edu.ec>
2023-01-18 15:23:19 -0500

    Importación de secureRandom

--- Ejemplos/src/paquete03/Principal.java
+++ Ejemplos/src/paquete03/Principal.java
@@ -7,6 +7,9 @@ package paquete03;
 
 import paquete04.OperadorUno;
 import paquete04.OperadorDos;
+import paquete05.OperadorTres;
+
+// import paquete04.*;
 
 /**
  *
@@ -17,10 +20,15 @@ public class Principal {
     public static void main(String[] args) {
         int valorA = OperadorUno.obtenerNumero(100);
         int valorB = OperadorUno.obtenerNumero(10);
+        int multiplicacion = OperadorTres.obtenerMultiplicacion(valorA, valorB);
         int suma = OperadorDos.obtenerSuma(valorA, valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n",
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La mutiplicación de %d x %d es igual a: %d\n",
                 valorA,
                 valorB,
-                suma);
+                suma,
+                valorA,
+                valorB,
+                multiplicacion);
     }
 }
